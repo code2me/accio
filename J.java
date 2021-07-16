79eec1eb590fafc42674a9ecfdca806695fcb2e2
Navneet Bahuguna <mr.navneet.bahuguna@gmail.com>
2021-07-16 14:44:40 +0530

    improve J.java code

--- J.java
+++ J.java
@@ -5,39 +5,40 @@ public class J {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int i, j, count = n;
 
-        while (n > 0) {
-            String s1;
-            String s2;
-            int m = n, l = n;
-            s1 = sc.next();
-            int l1 = s1.length();
-            for (int i = 0; i < n; i++) {
-                s2 = sc.next();
-                int l2 = s2.length();
-                int t = m;
-                int len = Math.min(l1, l2);
-                for (i = 0; i < len; i++) {
-                    if (s1.charAt(i) > s2.charAt(i)) {
-                        m--;
-                        break;
-                    } else if (s1.charAt(i) < s2.charAt(i)) {
-                        m++;
-                        break;
-                    }
-                }
-                if (t == m)
-                    m += l1 <= l2 ? 1 : -1;
-                s1 = s2;
-                l1 = l2;
-            }
-            sc.close();
-            if (2 * l == m + 1)
-                System.out.println("INCREASING\n");
-            else if (m == 1)
-                System.out.println("DECREASING\n");
-            else
-                System.out.println("NEITHER\n");
+        List<String> list = new ArrayList<String>();
+
+        for (i = 0; i < n; i++) {
+            list.add(sc.next());
         }
+        sc.close();
+
+        for (i = 1; i < n; i++) {
+            String s1 = list.get(i - 1);
+            String s2 = list.get(i);
+            int len1 = s1.length();
+            int len2 = s2.length();
+
+            int len = Math.min(len1, len2);
+
+            for (j = 0; j < len; j++) {
+                if (s1.charAt(j) > s2.charAt(j)) {
+                    count--;
+                    break;
+                } else if (s1.charAt(j) < s2.charAt(j)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        if (count == 2 * n - 1)
+            System.out.println("INCREASING\n");
+        else if (count == 1)
+            System.out.println("DECREASING\n");
+        else
+            System.out.println("NEITHER\n");
+
     }
 }
